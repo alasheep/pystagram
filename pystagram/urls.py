c08a86fa2764577e899637f20ba7a63938c442b4
alasheep <alasheep@hanmail.net>
2016-01-01 12:18:44 +0900

    build error modify

--- pystagram/urls.py
+++ pystagram/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from photo.views import single_photo
 
 urlpatterns = patterns('',
     url(r'^photo/(?P<photo_id>\d+)$','photo.views.single_photo', name='view_single_photo'),
